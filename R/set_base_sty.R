7063e355c2b0dfebcff29bdc9ae26c68fb436fb9
fkeck <francois.keck@gmail.com>
2017-11-22 22:02:57 +0100

    add some styles

--- R/set_base_sty.R
+++ R/set_base_sty.R
@@ -78,6 +78,8 @@ set_base_sty <- function(theme = NA, variation = 2){
                                                                  cex = 1,
                                                                  whisklty = "solid")),
                               image.default = list(col = colorRampPalette(c(col_bg, col_data))(12)),
+                              contour.default = list(col = col_data),
+                              dotchart = list(color = col_data, pch = 20, lcolor = par("fg")),
                               title = list(line = 2)))
 
   styles::style(ed_sty)
